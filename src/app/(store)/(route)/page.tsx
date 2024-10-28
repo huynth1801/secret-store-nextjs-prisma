28380e0bc1c7ae47505ab992fc794bff111bc068
huynth1801 <huuhuy1801@gmail.com>
2024-10-28 12:23:23 +0700

    fix type of banner item

--- src/app/(store)/(route)/page.tsx
+++ src/app/(store)/(route)/page.tsx
@@ -5,6 +5,13 @@ import { Separator } from "@/components/ui/separator"
 import prisma from "@/lib/prisma"
 import { isVariableValid } from "@/lib/utils"
 
+type Banner = {
+  id: string
+  image: string
+  title?: string
+  description?: string
+}
+
 export default async function Index() {
   const products = await prisma.product.findMany({
     include: {
@@ -17,7 +24,7 @@ export default async function Index() {
 
   return (
     <div className="flex flex-col border-neutral-200 dark:border-neutral-700">
-      <Carousel images={banners.map((obj) => obj.image)} />
+      <Carousel images={banners.map((obj: Banner) => obj.image)} />
       <Separator className="my-8" />
       <Heading title="Products" description="List of products" />
       {isVariableValid(products) ? (
